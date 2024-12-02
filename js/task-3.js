7f80e741f47d87a1b67323f334a2c5f632f2ab77
Sviatoslav Godyk <godyk1992@gmail.com>
2024-12-02 22:49:44 +0800

    add hw

---
+++ js/task-3.js
@@ -0,0 +1,21 @@
+const profile = {
+  name: "Jacob",
+  playtime: 300,
+  changeUsername(newName) {
+    this.name = newName;
+  },
+  getInfo() {
+    console.log(`${this.name} has ${this.playtime} active hours!`);
+  },
+};
+profile.getInfo();
+console.log(profile.changeUsername("Marco"));
+// console.log(profile.name);
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
